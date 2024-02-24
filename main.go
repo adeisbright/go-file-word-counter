ace2470b753c0c831e457d686a4a0a7292f6fa72
Adeleke Bright <bigjaratechnologies@gmail.com>
2024-02-24 07:53:51 +0100

    Improve the Read Function by Adding Check for Emptiness

--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/adeisbright/go-file-word-counter/reader"
 )
 
 func main() {
 	fmt.Println("Welcome to File Word Counter")
-	wordCount := reader.Read("Hello Bayo", "Bayo")
-	fmt.Println("Bayo occurs ", wordCount)
+	sentence := "The quick brown fox jumps over the lazy dog"
+	words := strings.Fields(sentence)
+	word := "dog"
+	wordCount, err := reader.Read(words, word)
+	if err != nil {
+		fmt.Println("Error occured", err)
+		return
+	}
+	fmt.Printf("The word '%s' appears %d times in the sentence.\n", word, wordCount)
 }
--- reader/read.go
+++ reader/read.go
@@ -1,14 +1,18 @@
 package reader
 
-import "strings"
+import (
+	"errors"
+)
 
-func Read(sentence string, text string) int {
+func Read(sentence []string, text string) (int, error) {
+	if text == "" || len(sentence) == 0 {
+		return 0, errors.New("sententce or words cannot be empty")
+	}
 	wordCounter := 0
-	words := strings.Fields(sentence)
-	for _, word := range words {
+	for _, word := range sentence {
 		if word == text {
 			wordCounter = wordCounter + 1
 		}
 	}
-	return wordCounter
+	return wordCounter, nil
 }
